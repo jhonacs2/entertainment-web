f434d5e6728dddbbc9dc01036deb06dbc807e068
Jhonatan Soto <jhonrocker2012@gmail.com>
2023-05-31 20:23:24 -0400

    finish login ui

--- src/app/shared/components/input/input.component.spec.ts
+++ src/app/shared/components/input/input.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { InputComponent } from './input.component';
+import {InputComponent} from './input.component';
 
 describe('InputComponent', () => {
   let component: InputComponent;
@@ -8,9 +8,9 @@ describe('InputComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ InputComponent ]
+      imports: [InputComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(InputComponent);
     component = fixture.componentInstance;
@@ -19,5 +19,34 @@ describe('InputComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not have label input after construction', () => {
+    expect(component.label).toBe('');
+  });
+
+  it('should have a placeholder called email', () => {
+    component.placeholder = 'email';
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('input').placeholder).toEqual('email');
+  });
+
+  it('should have a label called email', () => {
+    component.label = 'email';
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('label').textContent).toEqual('email');
+  });
+
+  it('should have a input with id email', () => {
+    component.idInput = 'email';
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('input').id).toEqual('email');
+  });
+
+  it('should have a label with for email', () => {
+    component.idInput = 'email';
+    fixture.detectChanges();
+    const labelHtmlElement = fixture.nativeElement.querySelector('label');
+    expect(labelHtmlElement.getAttribute('for')).toEqual('email');
   });
 });
--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,13 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-input',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
 })
-export class InputComponent {
+export class InputComponent implements OnInit {
+  @Input() placeholder: string = '';
+  @Input() required: boolean = false;
+  @Input() idInput: string = '';
+  @Input() label: string = '';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
 
 }
