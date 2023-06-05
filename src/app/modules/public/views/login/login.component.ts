cdd3908dc595fc611787289880640e065d8d04d5
Jhonatan Soto <jhonrocker2012@gmail.com>
2023-06-04 21:34:14 -0400

    add validators login

--- src/app/modules/public/views/login/login.component.ts
+++ src/app/modules/public/views/login/login.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +7,17 @@ import {FormBuilder} from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  submitted: boolean = false;
   loginForm = this._formBuilder.group({
-    email: [''],
-    password: ['']
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.required]]
   });
 
   constructor(private _formBuilder: FormBuilder) {
   }
 
   public onLogin(): void {
-    console.log(this.loginForm.value);
+    console.log(this.loginForm.status);
+    this.submitted = true;
   }
 }
--- src/app/shared/directives/e-input/e-input.directive.ts
+++ src/app/shared/directives/e-input/e-input.directive.ts
@@ -1,27 +1,33 @@
-import {AfterViewInit, ChangeDetectorRef, Directive, ElementRef, HostListener} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Directive, ElementRef, HostListener, Optional} from '@angular/core';
+import {NgModel} from '@angular/forms';
 
 @Directive({
-  selector: '[eInput]',
+  selector: '[eInputText]',
   standalone: true,
   host: {
-    class: 'input-container form_input form_label'
+    class: 'input-container form_input form_label ',
+    '[class.input-filled]': 'inputFilled'
   }
 })
 export class EInputDirective implements AfterViewInit {
+  inputFilled: boolean = false;
 
   @HostListener('input', ['$event'])
   onInput(): void {
     this.updateElement();
   }
 
-  constructor(public el: ElementRef, private cd: ChangeDetectorRef) {
+  constructor(public el: ElementRef,
+              private cd: ChangeDetectorRef,
+              @Optional() public ngModel: NgModel) {
   }
 
   ngAfterViewInit(): void {
     this.updateElement();
+    this.cd.detectChanges();
   }
 
   public updateElement(): void {
-    console.log(this.el.nativeElement.value);
+    this.inputFilled = (this.el.nativeElement.value || this.ngModel);
   }
 }
